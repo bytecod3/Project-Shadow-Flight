aef81166484b628b98ee4de2c0528d9ff063d684
bytecod3 <emwiti658@gmail.com>
2025-08-17 14:47:08 +0300

    ADDS voltage divider resolver

---
+++ python-scripts/voltage-divider-resolver.py
@@ -0,0 +1,14 @@
+"""
+@author Edwin Mwiti
+@brief This file helps experiment with different voltage divider values for 
+DC voltage measurement circuit
+"""
+
+def calculate_output_voltage(vin, r1, r2):
+
+    vo = (r2 / (r1 +r2 )) * vin
+    print(f"VIN: {vin}, R1: {r1}, R2: {r2}: VO: {vo} ")
+    return vo
+
+
+calculate_output_voltage(3.3, 100000, 36000)
