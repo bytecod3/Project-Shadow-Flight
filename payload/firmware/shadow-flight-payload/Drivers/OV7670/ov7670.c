f135506b494b79dcabc77f45e6ed1ffba0e62ff5
bytecod3 <emwiti658@gmail.com>
2026-06-06 13:38:47 +0300

    ADDS OV7670 camera registers

---
+++ payload/firmware/shadow-flight-payload/Drivers/OV7670/ov7670.c
@@ -0,0 +1,8 @@
+/*
+ * @file ov7670.c
+ *
+ * @date: Jun 5, 2026
+ * @author: edwin
+ */
+
+
---
+++ payload/firmware/shadow-flight-payload/Drivers/OV7670/ov7670.h
@@ -0,0 +1,13 @@
+/*
+ * @file ov7670.h
+ *
+ * @date: Jun 5, 2026
+ * @author: edwin
+ */
+
+#ifndef OV7670_OV7670_H_
+#define OV7670_OV7670_H_
+
+
+
+#endif /* OV7670_OV7670_H_ */
