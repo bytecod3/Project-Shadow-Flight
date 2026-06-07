e6ced3a732ef5edb04f31b7bd97df22b12166d70
bytecod3 <emwiti658@gmail.com>
2026-06-07 18:03:30 +0300

    ADDS ov7670 I2C init

--- payload/firmware/shadow-flight-payload/Drivers/OV7670/ov7670.c
+++ payload/firmware/shadow-flight-payload/Drivers/OV7670/ov7670.c
@@ -5,5 +5,104 @@
  * @author: edwin
  */
 
+
+#include <stdio.h>
+#include <main.h>
 #include <ov7670.h>
+#include "stm32f4xx_hal.h"
+
+#define QVGA_WIDTH 			(320)
+#define QVGA_HEIGHT 		(240)
+
+static DCMI_HandleTypeDef* sp_hdcmi;
+static DMA_HandleTypeDef* sp_hdma_dcmi;
+static I2C_HandleTypeDef* sp_hi2c;
+
+static uint32_t s_dest_address_continous_mode;
+static void(*s_cb_hsync)(uint32_t h);
+static void(*s_cb_vsync)(uint32_t h);
+static uint32_t s_current_h;
+static uint32_t s_current_v;
+
+/**
+ * @brief write 8 bit data to camera module
+ */
+HAL_StatusTypeDef ov7670_i2c_write(uint8_t reg_addr, uint8_t data) {
+
+	HAL_StatusTypeDef s;
+	do {
+		s = HAL_I2C_Mem_Write(
+				sp_hi2c,
+				OV7670_SLAVE_W_ADDRESS,
+				reg_addr,
+				I2C_MEMADD_SIZE_8BIT,
+				&data,
+				1,
+				100
+				);
+
+	} while (s != HAL_OK && 0); // todo: add retry logic
+
+	return s;
+}
+
+/**
+ * @brief read 8-bit data from camera module
+ */
+HAL_StatusTypeDef ov7670_i2c_read(uint8_t reg_addr, uint8_t* data) {
+	HAL_StatusTypeDef s;
+
+	do {
+		s = HAL_I2C_Mem_Read(
+				sp_hi2c,
+				OV7670_SLAVE_ADDRESS,
+				reg_addr,
+				I2C_MEMADD_SIZE_8BIT,
+				data,
+				1,
+				1000
+
+		);
+
+		s = HAL_I2C_Master_Transmit(
+				sp_hi2c,
+				OV7670_SLAVE_ADDRESS,
+				&reg_addr,
+				1,
+				100
+				);
+
+		s |= HAL_I2C_Master_Receive(
+				sp_hi2c,
+				OV7670_SLAVE_ADDRESS,
+				reg_addr,
+				I2C_MEMADD_SIZE_8BIT,
+				100);
+
+	} while (s != HAL_OK && 0); // todo: add definite retry logic
+
+	return s;
+}
+
+/* function definitions */
+void ov7670_init(DCMI_HandleTypeDef* p_hdcmi, DCMI_HandleTypeDef* p_hdma_dcmi, I2C_HandleTypeDef* p_hi2c) {
+	sp_hdcmi = p_hdcmi;
+	sp_hdma_dcmi = p_hdma_dcmi;
+	sp_hi2c = p_hi2c;
+
+	s_dest_address_continous_mode = 0;
+
+	/* wake up the camera */
+	HAL_GPIO_WritePin(CAM_RESET_GPIO_Port, CAM_RESET_Pin, GPIO_PIN_RESET);
+	HAL_Delay(100);
+	HAL_GPIO_WritePin(CAM_RESET_GPIO_Port, CAM_RESET_Pin, GPIO_PIN_SET);
+	HAL_Delay(100);
+
+
+
+}
+
+
+
+
 
--- payload/firmware/shadow-flight-payload/Drivers/OV7670/ov7670.h
+++ payload/firmware/shadow-flight-payload/Drivers/OV7670/ov7670.h
@@ -10,5 +10,21 @@
 
 #include <ov7670_regs.h>
 
+#define OV7670_SLAVE_ADDRESS (0x42)  /* 42 for write */
+
+void ov7670_init(DCMI_HandleTypeDef* p_hdcmi, DCMI_HandleTypeDef* p_hdma_dcmi, I2C_HandleTypeDef* p_hi2c);
+
+void ov7670_config(uint32_t mode);
+
+void ov7670_start_capture(uint32_t cap_mode, uint32_t dest_address);
+
+void ov7670_stop_capture();
+
+void ov7670_register_callback(void (*cb_hsync)(uint32_t h), void(*cb_vsync)(uint32_t v));
+
+/* i2c functions */
+HAL_StatusTypeDef ov7670_i2c_write(uint8_t reg_addr, uint8_t data);
+
+HAL_StatusTypeDef ov7670_i2c_read(uint8_t reg_addr, uint8_t* data);
 
 #endif /* OV7670_OV7670_H_ */
